82dff067baf8aa3dfff8d0fd9d4d447b15968356
Chris Wilson <chris+github@qwirx.com>
2012-03-30 16:10:25 +0100

    Rename .django templates to .email as requested.

--- mail_templated/tests.py
+++ mail_templated/tests.py
@@ -4,7 +4,7 @@ from mail_templated import EmailMessage
 
 class MailTemplatedTest(TestCase):
     def test_simple(self):
-        email = EmailMessage('simple.django',
+        email = EmailMessage('simple.email',
             {'foo': 'bar'}, to=['test@example.com'])
         
         self.assertEqual('kevin spacey', email.subject)
@@ -19,7 +19,7 @@ class MailTemplatedTest(TestCase):
     def test_body_only(self):
         # import pdb; pdb.set_trace()
         
-        email = EmailMessage('body_only.django',
+        email = EmailMessage('body_only.email',
             {'foo': 'bar'}, to=['test@example.com'])
         
         self.assertEqual('', email.subject)
@@ -29,10 +29,10 @@ class MailTemplatedTest(TestCase):
         self.assertEqual('Only bar.', email.body)
 
     def test_change_template_then_context(self):
-        email = EmailMessage('body_only.django',
+        email = EmailMessage('body_only.email',
             {'foo': 'bar'}, to=['test@example.com'])
         
-        email.template_name = 'simple.django'
+        email.template_name = 'simple.email'
         
         # writing to the context doesn't change anything
         # email.context['foo'] = 'baz'
@@ -43,10 +43,10 @@ class MailTemplatedTest(TestCase):
         self.assertEqual('Hello baz.', email.body)
 
     def test_change_context_then_template(self):
-        email = EmailMessage('simple.django',
+        email = EmailMessage('simple.email',
             {'foo': 'bar'}, to=['test@example.com'])
         
         email.context['foo'] = 'baz'
-        email.template_name = 'body_only.django'
+        email.template_name = 'body_only.email'
 
         self.assertEqual('Only baz.', email.body)
