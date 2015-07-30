f1e11baf7efe8f499e195d0a5a70c1e56e3c8bb7
Artem Rizhov <artem.rizhov@gmail.com>
2015-07-30 21:12:50 +0400

    Added tests

--- mail_templated/tests.py
+++ mail_templated/tests.py
@@ -1,7 +1,78 @@
+from django.core import mail
 from django.test import TestCase
 
 from . import send_mail
 
 
 class SendMailTestCase(TestCase):
-    pass
+
+    def test_plain(self):
+        send_mail(
+            'mail_templated_test/plain.tpl', {'name': 'User'},
+            'from@inter.net', ['to@inter.net'])
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.from_email, 'from@inter.net')
+        self.assertEqual(message.to, ['to@inter.net'])
+        self.assertEqual(message.subject, 'Hello User')
+        self.assertEqual(message.body,
+                         'User, this is a plain text message.')
+
+    def test_html(self):
+        send_mail(
+            'mail_templated_test/plain.html', {'name': 'User'},
+            'from@inter.net', ['to@inter.net'])
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'Hello User')
+        self.assertEqual(message.body,
+                         'User, this is an html message.')
+
+    def test_multipart(self):
+        send_mail(
+            'mail_templated_test/multipart.html', {'name': 'User'},
+            'from@inter.net', ['to@inter.net'])
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'Hello User')
+        self.assertEqual(message.body,
+                         'User, this is a plain text part.')
+        self.assertEqual(len(message.alternatives), 1)
+        self.assertEqual(message.alternatives[0][0],
+                         'User, this is an html part.')
+        self.assertEqual(message.alternatives[0][1], 'text/html')
+
+    def test_alternatives(self):
+        send_mail(
+            'mail_templated_test/plain.tpl', {'name': 'User'},
+            'from@inter.net', ['to@inter.net'],
+            alternatives=[('HTML alternative', 'text/html')])
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.from_email, 'from@inter.net')
+        self.assertEqual(message.to, ['to@inter.net'])
+        self.assertEqual(message.subject, 'Hello User')
+        self.assertEqual(message.body,
+                         'User, this is a plain text message.')
+        self.assertEqual(len(message.alternatives), 1)
+        self.assertEqual(message.alternatives[0][0],
+                         'HTML alternative')
+        self.assertEqual(message.alternatives[0][1], 'text/html')
+
+    def test_multipart_alternatives(self):
+        send_mail(
+            'mail_templated_test/multipart.html', {'name': 'User'},
+            'from@inter.net', ['to@inter.net'],
+            alternatives=[('HTML alternative', 'text/html')])
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'Hello User')
+        self.assertEqual(message.body,
+                         'User, this is a plain text part.')
+        self.assertEqual(len(message.alternatives), 2)
+        self.assertEqual(message.alternatives[0][0],
+                         'HTML alternative')
+        self.assertEqual(message.alternatives[0][1], 'text/html')
+        self.assertEqual(message.alternatives[1][0],
+                         'User, this is an html part.')
+        self.assertEqual(message.alternatives[1][1], 'text/html')
