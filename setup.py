75f9d5b3d7333c5338808f03233026341fb8f4e4
Artem Rizhov <artem.rizhov@gmail.com>
2016-01-20 02:57:10 +0400

    Release version 2.4.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-mail-templated',
-    version='2.4.2',
+    version='2.4.3',
     packages=find_packages(),
     include_package_data=True,
     author='Artem Rizhov',
