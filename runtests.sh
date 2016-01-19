fdb27c75929d9803cbd516cef046f6a92c027634
Artem Rizhov <artem.rizhov@gmail.com>
2016-01-20 02:19:01 +0400

    Add test of installation from pypi

--- runtests.sh
+++ runtests.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ $1 == '--install' ]] ; then
+  install=true
+else
+  install=false
+fi
+
 function test {
     v=$1
     pv=$2
@@ -10,7 +16,10 @@ function test {
 
     # Setup virtual environment for the specified Django version if absent.
     IFS='.' read v1 v2 <<< "$v"
-    env="../env$pv-$v"
+    env="../env/$pv-$v"
+    if $install ; then
+        env="../env/install-$pv-$v"
+    fi
     if [ ! -d $env ] ; then
         virtualenv --no-site-packages -p /usr/bin/python$pv $env
     fi
@@ -18,8 +27,17 @@ function test {
     source $env/bin/activate
     # Install or upgrade the required Django version.
     pip install -U "django>=$v1.$v2,<$v1.$(($v2+1))"
-
-    $env/bin/python$pv runtests.py
+    if $install ; then
+        pip install -U "django-mail-templated"
+        project_dir="../testproject"
+        mkdir $project_dir
+        django-admin startproject testproject $project_dir
+        sed -i -- 's/\(INSTALLED_APPS\s*=\s*\[\)/\1"mail_templated",/' $project_dir/testproject/settings.py
+        $project_dir/manage.py test mail_templated
+#        rm -r $project_dir
+    else
+        $env/bin/python$pv runtests.py
+    fi
 
     deactivate
     echo ""
