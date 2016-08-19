8584b2b8c71c9e6ce7e8c775144e99db6650aa9e
Artem Rizhov <artem.rizhov@gmail.com>
2016-08-20 02:12:44 +0400

    Add use of pyenv in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -53,8 +53,9 @@ function test {
     v=$1
     pv=$2
     cdir=`pwd`
-    echo "===================================================================="
+    echo ""
     echo "Testing with Django $v and Python $pv"
+    echo "===================================================================="
 
     activate $v $pv false
 
@@ -103,18 +104,30 @@ fi
 
 cd $DIR/..
 
-test "1.4" "2"
-test "1.5" "2"
-test "1.5" "3.4"
-test "1.6" "2"
-test "1.6" "3.4"
-test "1.7" "2"
-test "1.7" "3.4"
-test "1.8" "2"
-test "1.8" "3.4"
-test "1.9" "2"
-test "1.9" "3"
-test "1.10" "2"
-test "1.10" "3"
+p2v=`pyenv versions --bare | grep -P '^2.\d+.\d+$' | tail -n 1`
+p34v=`pyenv versions --bare | grep -P '^3.4.\d+$' | tail -n 1`
+p3v=`pyenv versions --bare | grep -P '^3.\d+.\d+$' | tail -n 1`
+export PYENV_VERSION="$p2v:$p3v:$p34v"
+
+echo ""
+echo "Python versions to be used: $p2v, $p34v, $p3v"
+echo "===================================================================="
+p2v=`echo $p2v | sed -r 's/\.[^.]+$//'`
+p34v=`echo $p34v | sed -r 's/\.[^.]+$//'`
+p3v=`echo $p3v | sed -r 's/\.[^.]+$//'`
+
+test "1.4" $p2v
+test "1.5" $p2v
+test "1.5" $p34v
+test "1.6" $p2v
+test "1.6" $p34v
+test "1.7" $p2v
+test "1.7" $p34v
+test "1.8" $p2v
+test "1.8" $p34v
+test "1.9" $p2v
+test "1.9" $p3v
+test "1.10" $p2v
+test "1.10" $p3v
 
 cd $current_dir
